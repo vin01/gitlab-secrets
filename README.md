9a0b9aea6ec3f89d7f5923e9bbcd73dd1bce601f
vin01 <vinc.i@protonmail.ch>
2021-09-21 11:18:22 +0200

    Add script and instructions

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # gitlab-secrets
-Retrieve all Ci/CD variables from all projects on Gitlab
+
+Retrieve Ci/CD variables from all projects on a Gitlab instance.
+
+This can be helpful for audits and penetration testing. Ci/CD systems have access to various secrets like package registries, API tokens, internal services and even production environments.
+
+Even CI tests like `terraform plan` for [infrastructure as code](https://docs.gitlab.com/ee/user/infrastructure/iac/) mean providing credentials for cloud services and APIs and can be [very risky](https://alex.kaskaso.li/post/terraform-plan-rce).
+
+# usage
+
+```
+NAME
+    gitlab-secrets.py - List ci/cd variables from all gitlab projects
+                        which the given token has access to.
+
+SYNOPSIS
+    gitlab-secrets.py URL TOKEN <flags>
+
+DESCRIPTION
+    url : str
+        URL to the gitlab instance
+    token: str
+        access token for authentication
+    pretty: bool
+        pretty print
+    nomask: bool
+        Do not mask values for variables.
+        Default behavior is to mask the values after 10 characters
+
+POSITIONAL ARGUMENTS
+    URL
+    TOKEN
+
+FLAGS
+    --pretty=PRETTY
+        Default: False
+    --nomask=NOMASK
+        Default: False
+```
+
+# You might also like
+
+- [Anatomy of a Cloud Infrastructure Attack via a Pull Request](https://goteleport.com/blog/hack-via-pull-request/)
+- [Authenticating and reading secrets with HashiCorp Vault](https://docs.gitlab.com/ee/ci/examples/authenticating-with-hashicorp-vault/)
---
+++ gitlab-secrets.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+
+import requests
+import fire
+
+ROOT_URL = ""
+PRETTY = False
+NOMASK = False
+
+def vars(url, token, pretty=False, nomask=False):
+    """List all ci/cd variables from all gitlab projects
+    which the given token has access to.
+
+    url : str
+        URL to the gitlab instance
+    token: str
+        access token for authentication
+    pretty: bool
+        pretty print
+    nomask: bool
+        Do not mask values for variables. Default behavior is to mask the values after 10 characters
+    """
+    global ROOT_URL
+    global PRETTY
+    global NOMASK
+    if not "&page=" in url:
+        ROOT_URL = url
+        PRETTY = pretty
+        NOMASK = nomask
+        resp = requests.get("%s/api/v4/projects?per_page=30&page=1" % (url),
+                            headers={"Authorization": "Bearer %s" % (token)})
+    else:
+        resp = requests.get(url,
+                            headers={"Authorization": "Bearer %s" % (token)})
+    if resp.ok:
+        for project in resp.json():
+            var_resp = requests.get("%s/api/v4/projects/%s/variables" % (ROOT_URL, project["id"]),
+                                    headers={"Authorization": "Bearer %s" % (token)})
+            if var_resp.ok:
+                variables = var_resp.json()
+                if variables:
+                    print("----")
+                    print("Variables for project %s:" %
+                          (project["path_with_namespace"]))
+                    for variable in variables:
+                        variable.pop("variable_type")
+                        if len(variable["value"]) > 10 and not NOMASK:
+                            variable["value"] = "%s***" % (
+                                variable["value"][0:10])
+                        if PRETTY:
+                            variable.pop("protected")
+                            print('{"key": %-22s| "value": %-14s| "masked": %-6s| "environment_scope": %s}' % (
+                                variable["key"], variable["value"], variable["masked"], variable["environment_scope"]
+                            ))
+                        else:
+                            print(variable)
+                        variable.pop
+                    print("----\n")
+        if resp.links.get("next"):
+            vars(resp.links["next"]["url"], token)
+    else:
+        print(resp.status_code, url)
+
+
+if __name__ == '__main__':
+    fire.Fire(vars)
